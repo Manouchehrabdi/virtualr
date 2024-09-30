247af0b67bfea17ea28c937bb17003d0504ae484
Manouchehrabdi <mabdi0258@gmail.com>
2024-09-30 12:57:37 +0330

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants";
 const Navbar = () => {
+  const
   return (
     <div>
       <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
-          <div className="flex justify-center items-center">
+          <div className="flex justify-between items-center">
             <div className="flex items-center flex-shrink-0">
               <img className="h-10 w10 mr-2" src={logo} alt="logo" />
               <span className="text-xl traking-tight">virtualLR</span>
@@ -17,6 +20,11 @@ const Navbar = () => {
                 </li>
               ))}
             </ul>
+            <div className="hidden lg:flex jusstify-center space-x-12 items-center">
+              <a href="#" className="py-2 px-3  border rounded-md">ورود</a>
+              <a href="#" className="py-2 px-3 border bg-gradient-to-r from-orange-500 to-orange-800 ">ثبت نام</a>
+
+            </div>
           </div>
         </div>
       </nav>
